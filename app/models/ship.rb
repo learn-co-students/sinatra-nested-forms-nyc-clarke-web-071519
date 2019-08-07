3dae4659743803490cffa407e76ade83c4f2c470
github username <github email address>
2019-08-07 14:06:24 -0400

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,28 @@
 class Ship
+
+    attr_reader :name, :type, :booty
+
+    @@all_ships = []
+
+    def initialize(args)
+        @name = args[:name]
+        @type = args[:type]
+        @booty = args[:booty]
+
+        @@all_ships << self
+    end
+
+    def self.all 
+        @@all_ships
+    end
+
+    def self.clear
+        @@all_ships = []
+    end
+
 end
+
+
+# The ship class should have name, type, and booty attributes, 
+# as well as a class method `.all` that returns all the ships 
+# and a class method `.clear` that deletes all ships.
